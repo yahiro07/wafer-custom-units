a2d79aa112bc4dfa77bfbf41b3b67bf059495446
yahiro <yahiro07@users.noreply.github.com>
2026-07-12 10:33:44 +0900

    change base url

--- ts/model-1/vite.config.ts
+++ ts/model-1/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ["lucide-react"],
