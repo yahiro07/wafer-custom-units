2fca1e0cc94edf6853de95bb6ccdfac51eb390f5
yahiro <yahiro07@users.noreply.github.com>
2026-07-14 18:53:34 +0900

    integrate unit

--- ts-beta/jw-guitar-amp/src/audio/effect-engine.js
+++ ts-beta/jw-guitar-amp/src/audio/effect-engine.js
@@ -1,122 +1,148 @@
-const volume = document.getElementById('volume');
-const treble = document.getElementById('treble');
-const mid = document.getElementById('mid');
-const bass = document.getElementById('bass');
-const visualizer = document.getElementById('visualizer');
-  
-const context = new AudioContext();
-const analyserNode = new AnalyserNode(context, { fftSize: 128 });
-const gainNode = new GainNode(context, { gain: volume.value });
-const trebleEQ = new BiquadFilterNode(context, {
-  type: 'highshelf',
-  frequency: 3000,
-  gain: treble.value
-});
-const midEQ = new BiquadFilterNode(context, {
-  // peak to freq. 1500 and gradually increase or decrease change
-  type: 'peaking',
-  // determines how far mid ranges span out - square root of 0.5 increments as not to clash with treble or bass
-  Q: Math.SQRT1_2,
-  frequency: 1500,
-  gain: mid.value
-});
-const bassEQ = new BiquadFilterNode(context, {
-  type: 'lowshelf',
-  frequency: 500,
-  gain: bass.value
-});
+const unitInterface = window.queryUnitInterface?.("wafer-v01");
 
+export function setupEffectEngine() {
+  const volume = document.getElementById("volume");
+  const treble = document.getElementById("treble");
+  const mid = document.getElementById("mid");
+  const bass = document.getElementById("bass");
+  const visualizer = document.getElementById("visualizer");
 
-const getGuitar = () => {
-  return navigator.mediaDevices.getUserMedia({
-    audio: {
-      echoCancellation: false,
-      autoGainControl: false,
-      noiseSuppression: false,
-      latency: 0
-    }
-  })
-};
+  const context = unitInterface?.audioContext ?? new AudioContext();
 
-const setupContext = async () => {
-  const guitar = await getGuitar();
+  const analyserNode = new AnalyserNode(context, { fftSize: 128 });
+  const gainNode = new GainNode(context, { gain: volume.value });
+  const trebleEQ = new BiquadFilterNode(context, {
+    type: "highshelf",
+    frequency: 3000,
+    gain: treble.value,
+  });
+  const midEQ = new BiquadFilterNode(context, {
+    // peak to freq. 1500 and gradually increase or decrease change
+    type: "peaking",
+    // determines how far mid ranges span out - square root of 0.5 increments as not to clash with treble or bass
+    Q: Math.SQRT1_2,
+    frequency: 1500,
+    gain: mid.value,
+  });
+  const bassEQ = new BiquadFilterNode(context, {
+    type: "lowshelf",
+    frequency: 500,
+    gain: bass.value,
+  });
 
-  if (context.state === 'suspended') {
-    await context.resume()
+  const getGuitar = () => {
+    return navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: false,
+        autoGainControl: false,
+        noiseSuppression: false,
+        latency: 0,
+      },
+    });
   };
 
-  const source = context.createMediaStreamSource(guitar)
-  source
-    .connect(trebleEQ)
-    .connect(midEQ)
-    .connect(bassEQ)
-    .connect(gainNode)
-    .connect(analyserNode)
-    .connect(context.destination)
-};
+  const setupConnectionChain = (source, destination) => {
+    source
+      .connect(trebleEQ)
+      .connect(midEQ)
+      .connect(bassEQ)
+      .connect(gainNode)
+      .connect(analyserNode)
+      .connect(destination);
+  };
 
-const drawVisualizer = () => {
-  requestAnimationFrame(drawVisualizer)
+  const setupContext = async () => {
+    const guitar = await getGuitar();
+    if (context.state === "suspended") {
+      await context.resume();
+    }
+    const source = context.createMediaStreamSource(guitar);
+    setupConnectionChain(source, context.destination);
+  };
 
-  // Number for how many different frequencies 
-  const bufferLength = analyserNode.frequencyBinCount;
-  // How many different freqs. are being returned from analyserNode
-  const dataArray = new Uint8Array(bufferLength);
-  // array is populated with value that corresponds with level of a particular frequency
-  analyserNode.getByteFrequencyData(dataArray);
+  const setupUnitSignals = () => {
+    setupConnectionChain(
+      unitInterface.audioInputNode,
+      unitInterface.audioOutputNode,
+    );
+  };
 
-  const width = visualizer.width;
-  const height = visualizer.height;
-  const barWidth = width / bufferLength
+  const drawVisualizer = () => {
+    requestAnimationFrame(drawVisualizer);
 
-  const canvasContext = visualizer.getContext('2d');
-  canvasContext.clearRect(0, 0, width, height);
+    // Number for how many different frequencies
+    const bufferLength = analyserNode.frequencyBinCount;
+    // How many different freqs. are being returned from analyserNode
+    const dataArray = new Uint8Array(bufferLength);
+    // array is populated with value that corresponds with level of a particular frequency
+    analyserNode.getByteFrequencyData(dataArray);
 
-  dataArray.forEach((item, index) => {
-    // 255 is max number for frequency
-    const y = item / 255 * height / 2;
-    // width of each individual bar multiplied by current index 
-    const x = barWidth * index;
-    // staying within hue of 200 to keep blue colors 
-    // '* 400' to compensate for the  '/ 2' on line 53
-    canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
-    // to draw - starting at x position || y position is height - y
-    canvasContext.fillRect(x, height - y, barWidth, y)
-  })
-};
+    const width = visualizer.width;
+    const height = visualizer.height;
+    const barWidth = width / bufferLength;
 
-const resize = () => {
-  visualizer.width = visualizer.clientWidth * window.devicePixelRatio
-  visualizer.height = visualizer.clientHeight * window.devicePixelRatio
-};
+    const canvasContext = visualizer.getContext("2d");
+    canvasContext.clearRect(0, 0, width, height);
 
-const setupEventListeners = () => {
-  window.addEventListener('resize', resize);
+    dataArray.forEach((item, index) => {
+      // 255 is max number for frequency
+      const y = ((item / 255) * height) / 2;
+      // width of each individual bar multiplied by current index
+      const x = barWidth * index;
+      // staying within hue of 200 to keep blue colors
+      // '* 400' to compensate for the  '/ 2' on line 53
+      canvasContext.fillStyle = `hsl(${(y / height) * 400}, 100%, 50%)`;
+      // to draw - starting at x position || y position is height - y
+      canvasContext.fillRect(x, height - y, barWidth, y);
+    });
+  };
 
-  volume.addEventListener('input', e => {
-    const value = parseFloat(e.target.value)
-    // preventing clipping of audio when volume is changed drastically in a quick motion
-    // value = where we want to be, currentTime = start time, .01 = change (incremental) value
-    gainNode.gain.setTargetAtTime(value, context.currentTime, .01)
-  });
+  const resize = () => {
+    visualizer.width = visualizer.clientWidth * window.devicePixelRatio;
+    visualizer.height = visualizer.clientHeight * window.devicePixelRatio;
+  };
 
-  treble.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    trebleEQ.gain.setTargetAtTime(value, context.currentTime, .01)
-  });
+  const setupEventListeners = () => {
+    window.addEventListener("resize", resize);
 
-  mid.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    midEQ.gain.setTargetAtTime(value, context.currentTime, .01)
-  });
+    volume.addEventListener("input", (e) => {
+      const value = parseFloat(e.target.value);
+      // preventing clipping of audio when volume is changed drastically in a quick motion
+      // value = where we want to be, currentTime = start time, .01 = change (incremental) value
+      gainNode.gain.setTargetAtTime(value, context.currentTime, 0.01);
+    });
 
-  bass.addEventListener('input', e => {
-    const value = parseInt(e.target.value)
-    bassEQ.gain.setTargetAtTime(value, context.currentTime, .01)
+    treble.addEventListener("input", (e) => {
+      const value = parseInt(e.target.value);
+      trebleEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+    });
+
+    mid.addEventListener("input", (e) => {
+      const value = parseInt(e.target.value);
+      midEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+    });
+
+    bass.addEventListener("input", (e) => {
+      const value = parseInt(e.target.value);
+      bassEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+    });
+  };
+
+  setupEventListeners();
+  if (unitInterface) {
+    setupUnitSignals();
+  } else {
+    setupContext();
+  }
+  resize();
+  drawVisualizer();
+
+  unitInterface?.completeSetup({
+    unitAspects: {
+      unitType: "effect",
+      outputs: ["audio"],
+      inputs: ["audio"],
+      viewSize: [860, 520],
+    },
   });
 }
-
-setupEventListeners();
-setupContext();
-resize();
-drawVisualizer();
