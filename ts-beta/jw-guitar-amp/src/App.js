d391dc5d7dbb5d488f83659a79d4454adebe2177
yahiro <yahiro07@users.noreply.github.com>
2026-07-14 18:49:45 +0900

    adjust code format

--- ts-beta/jw-guitar-amp/src/App.js
+++ ts-beta/jw-guitar-amp/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Header from './components/Header';
-import Mixer from './components/Mixer';
+import "./App.css";
+import Header from "./components/Header";
+import Mixer from "./components/Mixer";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Mixer />  
+      <Mixer />
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- ts-beta/jw-guitar-amp/src/components/Header.js
+++ ts-beta/jw-guitar-amp/src/components/Header.js
@@ -1,17 +1,14 @@
 // Dependencies
-import React from 'react';
-import logo from '../images/logo/logo-gray.jpg';
+import React from "react";
+import logo from "../images/logo/logo-gray.jpg";
 
 // Styles
-import '../styles/Header.css';
+import "../styles/Header.css";
 
 export default function Header() {
   return (
-    <div className='header'>
-      <img 
-        src={logo} 
-        alt='logo'
-      />
+    <div className="header">
+      <img src={logo} alt="logo" />
     </div>
-  )
-};
+  );
+}
