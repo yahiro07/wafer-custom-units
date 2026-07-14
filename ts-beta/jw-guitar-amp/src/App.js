f47a8ae3df1ab3e89fbd18dd3e387b9e970193c4
yahiro <yahiro07@users.noreply.github.com>
2026-07-14 18:46:47 +0900

    add project

---
+++ ts-beta/jw-guitar-amp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Mixer from './components/Mixer';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Mixer />  
+    </div>
+  )
+};
+
+export default App;
