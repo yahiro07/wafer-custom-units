af15c6570226e0bac6501e53ebeab4936a2f03b2
yahiro <yahiro07@users.noreply.github.com>
2026-07-15 09:24:19 +0900

    add build script

---
+++ ts-beta/build_all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dstDir=./dist
+rm -rf "$dstDir"
+mkdir -p "$dstDir"
+cd ./jw-guitar-amp && pnpm run build && cd ..
+cd ./model-1 && pnpm run build && cd ..
+cd ./poly-synth && pnpm run build && cd ..
+cd ./react-synth && pnpm run build && cd ..
+cd ./sk-synth && pnpm run build && cd ..
+cd ./super-oscillator && pnpm run build && cd ..
+cd ./vue-synth && pnpm run build && cd ..
+
+echo "build done."
