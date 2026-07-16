0706cc852f40b874ddeb2874867fb9e5629fdaa2
yahiro <yahiro07@users.noreply.github.com>
2026-07-16 12:38:52 +0900

    format code

--- vanilla-beta/darkwave/scripts/Param.js
+++ vanilla-beta/darkwave/scripts/Param.js
@@ -1,6 +1,4 @@
-
 //  Copyright © 2020–2021, Stewart Smith. See LICENSE for details.
-
 
 /*
 
@@ -16,243 +14,165 @@
 
 */
 
+import Bank from "./Bank.js";
 
+function Param(waveType, frequencyFactor, volumeFactor) {
+  //  Keep a refence to the local “this”,
+  //  mark this instance’s index #,
+  //  and store this param in an array
+  //  with its siblings.
 
+  const param = this;
+  this.index = Param.all.length;
+  Param.all.push(this);
 
-import Bank from './Bank.js'
+  //  Begin build-out of the user interface.
 
+  const template = document.getElementById("param"),
+    clone = template.content.cloneNode(true),
+    paramsTable = document.querySelector("#params table");
 
+  paramsTable.appendChild(clone);
+  this.domElement = paramsTable.querySelector("tr.param:last-child");
 
+  this.domElement.querySelector(".param-index").innerText = this.index + 1;
 
-function Param( waveType, frequencyFactor, volumeFactor ){
+  this.setWaveType(waveType, true);
+  this.setFrequencyFactor(frequencyFactor, true);
+  this.setVolumeFactor(volumeFactor, true);
 
+  //  Add on-change event listeners
+  //  for changes to this param’s
+  //  waveType, frequencyFactor, volumeFactor.
+  ["waveType", "frequencyFactor", "volumeFactor"].forEach(function (name) {
+    param.domElement
+      .querySelector(`[name=${name}]`)
+      .addEventListener("change", function (event) {
+        param["set" + name[0].toUpperCase() + name.substr(1)](
+          event.target.value,
+        );
+      });
+  });
 
-	//  Keep a refence to the local “this”,
-	//  mark this instance’s index #,
-	//  and store this param in an array
-	//  with its siblings.
+  //  Enable the “Remove” button
+  //  for this param.
 
-	const param = this
-	this.index = Param.all.length
-	Param.all.push( this )
+  this.domElement
+    .querySelector(".param-remove")
+    .addEventListener("mousedown", function () {
+      Param.remove(param);
+    });
 
-
-	//  Begin build-out of the user interface.
-
-	const 
-	template = document.getElementById( 'param' ),
-	clone = template.content.cloneNode( true ),
-	paramsTable = document.querySelector( '#params table' )
-	
-	paramsTable.appendChild( clone )
-	this.domElement = paramsTable
-	.querySelector( 'tr.param:last-child' )
-
-	this.domElement
-	.querySelector( '.param-index' )
-	.innerText = this.index + 1
-
-
-
-
-	this.setWaveType( waveType, true )
-	this.setFrequencyFactor( frequencyFactor, true )
-	this.setVolumeFactor( volumeFactor, true )
-
-
-	//  Add on-change event listeners
-	//  for changes to this param’s
-	//  waveType, frequencyFactor, volumeFactor.
-
-	;[
-
-		'waveType',
-		'frequencyFactor',
-		'volumeFactor'
-	
-	].forEach( function( name ){
-
-		param
-		.domElement
-		.querySelector( `[name=${ name }]` )
-		.addEventListener( 'change', function( event ){
-
-			param[ 'set' + name[ 0 ].toUpperCase()
-			+ name.substr( 1 )]( event.target.value )
-		})
-	})
-
-
-	//  Enable the “Remove” button
-	//  for this param.
-
-	this.domElement
-	.querySelector( '.param-remove' )
-	.addEventListener( 'mousedown', function(){
-
-		Param.remove( param )
-	})
-
-
-
-
-	Bank.all.addVoice( this )
+  Bank.all.addVoice(this);
 }
 
+Object.assign(Param, {
+  all: [],
+  remove: function (param) {
+    const index = this.all.indexOf(param);
+    param.domElement.remove();
+    Param.all.splice(index, 1);
+    Param.all.forEach(function (param, i) {
+      param.index = i;
+      param.domElement.querySelector(".param-index").innerText = i + 1;
+    });
+    Bank.all.removeVoice(index);
+    this.updateUrlFromParams();
+  },
+  updateUrlFromParams: function () {
+    history.replaceState(
+      {},
 
+      "Darkwave.js",
+      Param.all.reduce(function (hash, param, i) {
+        return (
+          hash +
+          (i > 0 ? ";" : "") +
+          param.waveType +
+          "," +
+          param.frequencyFactor +
+          "," +
+          param.volumeFactor
+        );
+      }, "#"),
+    );
+  },
+  parseUrlForParams: function () {
+    let hash = document.location.hash;
+    if (hash.length === 0) {
+      //hash = '#square,1/2,0.8;triangle,3/4,0.2;sine,1/1,0.3'
+      hash = "#square,0.5,0.3;triangle,0.75,0.2;sine,1,0.1";
+    }
 
+    hash
+      .substr(1)
+      .split(";")
+      .forEach(function (args) {
+        new Param(...args.split(","));
+      });
+  },
+});
 
-Object.assign( Param, {
+Object.assign(Param.prototype, {
+  setWaveType: function (waveType, skipUpdate) {
+    if (
+      ["sine", "square", "sawtooth", "triangle"].includes(waveType) !== true
+    ) {
+      waveType = "square";
+    }
 
-	all: [],
-	remove: function( param ){
-
-		const index = this.all.indexOf( param )
-		param.domElement.remove()
-		Param.all.splice( index, 1 )
-		Param.all.forEach( function( param, i ){
-
-			param.index = i
-			param.domElement
-			.querySelector( '.param-index' )
-			.innerText = ( i + 1 )
-		})
-		Bank.all.removeVoice( index )
-		this.updateUrlFromParams()
-	},
-	updateUrlFromParams: function(){
-
-		history.replaceState({}, 
-
-			'Darkwave.js', 
-			Param.all
-			.reduce( function( hash, param, i ){
-
-				return (
-
-					hash
-					+ ( i > 0 ? ';' : '' )
-					+ param.waveType +','
-					+ param.frequencyFactor +','
-					+ param.volumeFactor
-				)
-
-			}, '#' )
-		)
-	},
-	parseUrlForParams: function(){
-
-		let hash = document.location.hash
-		if( hash.length === 0 ){
-
-			//hash = '#square,1/2,0.8;triangle,3/4,0.2;sine,1/1,0.3'
-			hash = '#square,0.5,0.3;triangle,0.75,0.2;sine,1,0.1'
-		}
-
-		hash
-		.substr( 1 )
-		.split( ';' )
-		.forEach( function( args ){
-
-			new Param( ...args.split( ',' ))
-		})
-	}
-})
-
-
-
-
-Object.assign( Param.prototype, {
-
-	setWaveType: function( waveType, skipUpdate ){
-		
-		if([
-
-			'sine',
-			'square',
-			'sawtooth',
-			'triangle'
-
-		].includes( waveType ) !== true ){
-
-			waveType = 'square'
-		}
-
-		this.waveType = waveType
-		this
-		.domElement
-		.querySelector( `
+    this.waveType = waveType;
+    this.domElement
+      .querySelector(
+        `
 
 			select[name='waveType']
-			option[value='${ waveType }']
+			option[value='${waveType}']
 
-		` )
-		.setAttribute( 'selected', true )
-		if( skipUpdate !== true ){
+		`,
+      )
+      .setAttribute("selected", true);
+    if (skipUpdate !== true) {
+      Bank.all.setWaveTypeForVoice(this.index, waveType);
+      Param.updateUrlFromParams();
+    }
+  },
+  setFrequencyFactor: function (frequencyFactor, skipUpdate) {
+    frequencyFactor = parseFloat(frequencyFactor);
+    if (typeof frequencyFactor !== "number" || isNaN(frequencyFactor))
+      frequencyFactor = 1;
 
-			Bank.all.setWaveTypeForVoice( this.index, waveType )
-			Param.updateUrlFromParams()
-		}
-	},
-	setFrequencyFactor: function( frequencyFactor, skipUpdate ){
+    this.frequencyFactor = frequencyFactor;
+    this.domElement.querySelector("[name=frequencyFactor]").value =
+      parseFloat(frequencyFactor);
+    if (skipUpdate !== true) {
+      Bank.all.setFrequencyFactorForVoice(this.index, frequencyFactor);
+      Param.updateUrlFromParams();
+    }
+  },
+  setVolumeFactor: function (volumeFactor, skipUpdate) {
+    volumeFactor = parseFloat(volumeFactor);
+    if (typeof volumeFactor !== "number" || isNaN(volumeFactor))
+      volumeFactor = 0.1;
 
-		frequencyFactor = parseFloat( frequencyFactor )
-		if( typeof frequencyFactor !== 'number' 
-			|| isNaN( frequencyFactor )) frequencyFactor = 1
+    this.volumeFactor = volumeFactor;
+    this.domElement.querySelector("[name=volumeFactor]").value =
+      parseFloat(volumeFactor);
+    if (skipUpdate !== true) {
+      Bank.all.setVolumeFactorForVoice(this.index, volumeFactor);
+      Param.updateUrlFromParams();
+    }
+  },
+});
 
-		this.frequencyFactor = frequencyFactor
-		this
-		.domElement
-		.querySelector( '[name=frequencyFactor]' )
-		.value = parseFloat( frequencyFactor )
-		if( skipUpdate !== true ){
+window.addEventListener("DOMContentLoaded", function () {
+  Param.parseUrlForParams();
 
-			Bank.all.setFrequencyFactorForVoice( this.index, frequencyFactor )
-			Param.updateUrlFromParams()
-		}		
-	},
-	setVolumeFactor: function( volumeFactor, skipUpdate ){
+  document
+    .getElementById("param-add")
+    .addEventListener("mousedown", function () {
+      new Param();
+    });
+});
 
-		volumeFactor = parseFloat( volumeFactor )
-		if( typeof volumeFactor !== 'number' 
-			|| isNaN( volumeFactor )) volumeFactor = 0.1
-
-		this.volumeFactor = volumeFactor
-		this
-		.domElement
-		.querySelector( '[name=volumeFactor]' )
-		.value = parseFloat( volumeFactor )
-		if( skipUpdate !== true ){
-		
-			Bank.all.setVolumeFactorForVoice( this.index, volumeFactor )
-			Param.updateUrlFromParams()
-		}
-	}
-})
-
-
-
-
-window.addEventListener( 'DOMContentLoaded', function(){
-
-	Param.parseUrlForParams()
-
-	document
-	.getElementById( 'param-add' )
-	.addEventListener( 'mousedown', function(){
-
-		new Param()
-	})
-})
-
-
-
-
-export default Param
-
-
-
-
-
-
-
+export default Param;
--- vanilla-beta/darkwave/scripts/peakClassifier.js
+++ vanilla-beta/darkwave/scripts/peakClassifier.js
@@ -1,82 +1,49 @@
-
-
-
-
 // peakClassifier( frequencyData, 0.8 )
 // peakClassifierMax( frequencyData, 0.8 )
 
-bufferLength = analyser.frequencyBinCount
+bufferLength = analyser.frequencyBinCount;
 
+const peakClassifier = function (peakThreshold) {
+    const kernelWidth = 2, //  Try 4, 6, 8, for different peakiness sensitivity.
+      peaks = [];
 
+    for (let i = kernelWidth; i < bufferLength - kernelWidth; i++) {
+      const bin = frequencyData[i];
 
+      let kernelAvgL = 0,
+        kernelAvgR = 0;
 
-const 
-peakClassifier = function( peakThreshold ){
+      for (let k = 1; k < kernelWidth; k++) {
+        kernelAvgL += bin - frequencyData[i - k]; //  Look at lower Hz.
+        kernelAvgR += bin - frequencyData[i + k]; //  Look at higher Hz.
+      }
+      kernelAvgL /= k;
+      kernelAvgR /= k;
 
-	const 
-	kernelWidth = 2,//  Try 4, 6, 8, for different peakiness sensitivity.
-	peaks = []
-	
-	for(
+      const kernelSum = ((kernelMaxL + kernelMaxR) * 0.5) / bin;
 
-		let i = kernelWidth;
-		i < bufferLength - kernelWidth; 
-		i ++ ){
+      if (kernelSum >= peakThreshold) peaks.push(i);
+    }
+    return peaks;
+  },
+  peakClassifierMax = function (frames, peakThreshold) {
+    const kernelWidth = 2; //  Try 4, 6, 8, for different peakiness sensitivity.
+    for (let t = 0; t < frames.length; t++) {
+      const fft = frames[t]; //.getFFT()
+      for (let bin = kernelWidth; bin < fft.length - kernelWidth; bin++) {
+        const kernelMaxL = -1000, //  init arbitrarily min number.
+          kernelMaxR = -1000, //  init arbitrarily min number.
+          kernelSum = 0;
 
-		const 
-		bin = frequencyData[ i ]
-
-		let
-		kernelAvgL = 0,
-		kernelAvgR = 0
-
-		for( let k = 1; k < kernelWidth; k ++ ){
-		
-			kernelAvgL += bin - frequencyData[ i - k ]//  Look at lower Hz.
-			kernelAvgR += bin - frequencyData[ i + k ]//  Look at higher Hz.
-		}
-		kernelAvgL /= k
-		kernelAvgR /= k
-		
-		const kernelSum = (( kernelMaxL + kernelMaxR ) * 0.5 ) / bin
-
-		if( kernelSum >= peakThreshold ) peaks.push( i )
-	}
-	return peaks
-},
-peakClassifierMax = function( frames, peakThreshold ){
-
-	const kernelWidth = 2//  Try 4, 6, 8, for different peakiness sensitivity.
-	for( let t = 0; t < frames.length; t ++ ){
-
-		const fft = frames[ t ]//.getFFT()
-		for( 
-			
-			let bin = kernelWidth; 
-			bin < fft.length - kernelWidth;
-			bin ++ ){
-
-			const
-			kernelMaxL = -1000,//  init arbitrarily min number.
-			kernelMaxR = -1000,//  init arbitrarily min number.
-			kernelSum  =     0
-
-			for( int k = 1; k < kernelWidth; k ++ ){
-				
-				kernelMaxL = max( kernelMaxL, bin - specs[ bin - k ].amp )//  Look at lower Hz.
-				kernelMaxR = max( kernelMaxR, bin - specs[ bin + k ].amp )//  Look at higher Hz.
-			}
-			kernelSum = (( kernelMaxL + kernelMaxR ) * 0.5 ) / fft[ bin ].amp
-			if( kernelSum >= peakThreshold ) fft[ bin ].peak = true
-
-		}//  for bin.
-	}//  for t.
-}
-
-
-
-
-
+        for (let k = 1; k < kernelWidth; k++) {
+          kernelMaxL = max(kernelMaxL, bin - specs[bin - k].amp); //  Look at lower Hz.
+          kernelMaxR = max(kernelMaxR, bin - specs[bin + k].amp); //  Look at higher Hz.
+        }
+        kernelSum = ((kernelMaxL + kernelMaxR) * 0.5) / fft[bin].amp;
+        if (kernelSum >= peakThreshold) fft[bin].peak = true;
+      } //  for bin.
+    } //  for t.
+  };
 
 /*
   void peakClassifier( Frame[] frms, float peakThresh ){
@@ -110,14 +77,6 @@ peakClassifierMax = function( frames, peakThreshold ){
         }// for frames(t)          
  };
  */
- 
-
-
-
-
-
-
-
 
 /*
  
@@ -150,4 +109,3 @@ peakClassifierMax = function( frames, peakThreshold ){
 */
 
 //  (*) "look backwards" and "look forwards" are *not* in time, but in FFT bin array space
-
