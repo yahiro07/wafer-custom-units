db8f972f33f076db512488428438010f07371781
yahiro <yahiro07@users.noreply.github.com>
2026-07-16 11:50:59 +0900

    integrate unit feature

--- ts-beta/simple-synth/js/synth.js
+++ ts-beta/simple-synth/js/synth.js
@@ -1,10 +1,31 @@
 var Synth = {};
 
+const waferToneSynthBridge = createWaferToneSynthBridge();
+
 document.addEventListener("DOMContentLoaded", function (event) {
   "use strict";
 
-  window.synth = new Synth.Instrument().toMaster();
+  window.synth = new Synth.Instrument().connect(
+    waferToneSynthBridge.destinationNode,
+  );
   Synth.UI(window.synth);
+
+  waferToneSynthBridge.unitInterface?.completeSetup({
+    unitAspects: {
+      unitType: "instrument",
+      outputs: ["audio"],
+      inputs: ["note"],
+      viewSize: [810, 410],
+    },
+    noteInput: waferToneSynthBridge.createNotePortAdapted({
+      triggerAttack(freq) {
+        window.synth.triggerAttack(freq);
+      },
+      triggerRelease() {
+        window.synth.triggerAttack();
+      },
+    }),
+  });
 
   // iOS support
   StartAudioContext(Tone.context, "#piano *");
