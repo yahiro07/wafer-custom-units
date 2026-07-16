640359b95a22a57cd200b60eb2af3df9bac3753d
yahiro <yahiro07@users.noreply.github.com>
2026-07-16 17:09:28 +0900

    update build script

--- ts-beta/build_all.sh
+++ ts-beta/build_all.sh
@@ -5,12 +5,15 @@ set -euo pipefail
 dstDir=./dist
 rm -rf "$dstDir"
 mkdir -p "$dstDir"
+cd ./aura && pnpm run build && cd ..
+cd ./cadence && pnpm run build && cd ..
 cd ./jw-guitar-amp && pnpm run build && cd ..
 cd ./model-1 && pnpm run build && cd ..
 cd ./poly-synth && pnpm run build && cd ..
 cd ./react-synth && pnpm run build && cd ..
 cd ./sk-synth && pnpm run build && cd ..
 cd ./super-oscillator && pnpm run build && cd ..
+cd ./syntho && pnpm run build && cd ..
 cd ./vue-synth && pnpm run build && cd ..
 
 echo "build done."
