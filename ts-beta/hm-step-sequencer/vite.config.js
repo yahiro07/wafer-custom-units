aadc62ac3a127c3b25cd7e4e4819cef0e132a84b
yahiro <yahiro07@users.noreply.github.com>
2026-07-21 12:55:06 +0900

    use pnpm workspace

--- ts-beta/hm-step-sequencer/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "./",
-  build: { outDir: "../dist/hm-step-sequencer", emptyOutDir: true },
-  server: { port: 3000 },
-});
--- ts-beta/super-oscillator/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './',
-  build: { outDir: '../dist/super-oscillator', emptyOutDir: true },
-  server: { port: 3000 },
-});
