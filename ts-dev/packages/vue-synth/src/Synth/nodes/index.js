4116724bd3817a88335a54e29a0a60caf0d3917c
yahiro <yahiro07@users.noreply.github.com>
2026-07-21 20:52:11 +0900

    remove units in development

--- ts-dev/packages/vue-synth/src/Synth/nodes/index.js
+++
@@ -1,9 +0,0 @@
-import LFO from "./LFO";
-import VCO from "./VCO";
-import FILTER from "./FILTER";
-import MODULATION from "./mod";
-import FX from "./FX";
-import DISTORTION from "./Distortion";
-import ENVELOPE from "./ENVELOPE";
-
-export { LFO, VCO, FILTER, MODULATION, FX, DISTORTION, ENVELOPE };
