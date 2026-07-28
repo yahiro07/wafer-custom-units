0c85df5324d43eb2e2b47d13fc2135ffeb5eeaed
yahiro <yahiro07@users.noreply.github.com>
2026-07-28 16:14:05 +0900

    apply persistence

--- js/webaudio-tinysynth-simple/app.js
+++ js/webaudio-tinysynth-simple/app.js
@@ -1,37 +1,59 @@
-var curProg = 0;
-var curOct = 0;
-var curNote = 60;
-var curMidi = 0;
-var midiPort = [];
-var currentPort = -1;
-
-function Init() {
+async function Init() {
   synth = document.getElementById("tinysynth");
   kb = document.getElementById("kb");
   kb.addEventListener("change", KeyIn);
 
-  synth.ready().then(() => {
-    for (var i = 0; i < 128; ++i) {
-      var o = document.createElement("option");
-      o.innerHTML = i + 1 + " : " + synth.getTimbreName(0, i);
-      document.getElementById("prog").appendChild(o);
-    }
-    ProgChange(0);
-  });
-}
+  await synth.ready();
 
-function MidiIn(e) {
-  if (synth) {
-    switch (e.data[0] & 0xf0) {
-      case 0x90:
-        kb.setNote(e.data[2] ? 1 : 0, e.data[1]);
-        break;
-      case 0x80:
-        kb.setNote(0, e.data[1]);
-    }
-    e.data[1] = e.data[1] + curOct * 12;
-    synth.send(e.data, 0);
+  for (let i = 0; i < 128; ++i) {
+    const o = document.createElement("option");
+    o.innerHTML = i + 1 + " : " + synth.getTimbreName(0, i);
+    document.getElementById("prog").appendChild(o);
   }
+  ProgChange(0);
+
+  unitInterface?.completeSetup({
+    unitAspects: {
+      unitType: "instrument",
+      categoryHint: "synthesizer",
+      viewSize: [580, 280],
+    },
+    noteInput: {
+      noteOn(noteNumber, time, velocity) {
+        synth.send([0x90, noteNumber, (velocity ?? 1) * 127], time);
+      },
+      noteOff(noteNumber, time) {
+        synth.send([0x80, noteNumber, 0], time);
+      },
+    },
+    persistence: {
+      emitStateBytes() {
+        const pg = document.getElementById("prog").selectedIndex;
+        const master = document.getElementById("vol").value;
+        const reverb = document.getElementById("rev").value;
+        const quality = synth.quality;
+        return new Uint8Array([
+          pg,
+          Math.round(master * 100),
+          Math.round(reverb * 100),
+          quality,
+        ]);
+      },
+      applyStateBytes(stateBytes) {
+        if (stateBytes.length !== 4) return;
+        const pg = stateBytes[0];
+        const master = stateBytes[1] / 100;
+        const reverb = stateBytes[2] / 100;
+        const quality = stateBytes[3];
+        document.getElementById("prog").selectedIndex = pg;
+        document.getElementById("vol").value = master;
+        document.getElementById("rev").value = reverb;
+        Prog(pg);
+        Ctrl();
+        SetQuality(quality);
+      },
+    },
+  });
 }
 
 function Ctrl() {
@@ -42,15 +64,14 @@ function Ctrl() {
 }
 
 function KeyIn(e) {
-  curNote = e.note[1] + curOct * 12;
-  if (e.note[0]) synth.send([0x90 + curMidi, curNote, 100]);
-  else synth.send([0x80 + curMidi, curNote, 0]);
+  const curOct = 0;
+  const curNote = e.note[1] + curOct * 12;
+  if (e.note[0]) synth.send([0x90, curNote, 100]);
+  else synth.send([0x80, curNote, 0]);
 }
 
 function ProgChange(p) {
-  if (synth) {
-    synth.send([0xc0, p]);
-  }
+  synth.send([0xc0, p]);
 }
 
 function SetQuality(n) {
