f6263b71854aca189a203558afa618bdf6a0956f
yahiro <yahiro07@users.noreply.github.com>
2026-08-03 15:24:19 +0900

    update build script

--- build_all.sh
+++ build_all.sh
@@ -2,14 +2,11 @@
 
 set -euo pipefail
 
-dstDir=./dist
-rm -rf "$dstDir"
-mkdir -p "$dstDir"
-cd ./threejs-audio-reactive-visual && pnpm install && pnpm run build && cd ..
-cp -r threejs-audio-reactive-visual/dist "$dstDir/threejs-audio-reactive-visual"
-cp -r additive/web "$dstDir/additive"
-cp -r wasyn-1/web "$dstDir/wasyn-1"
-cp -r webaudio-synth-v2/web "$dstDir/webaudio-synth-v2"
-cp -r webaudio-tinysynth-simple/web "$dstDir/webaudio-tinysynth-simple"
-cp -r vissonance/web "$dstDir/vissonance"
-echo "copy done."
+cd ./ts && pnpm install && pnpm run build && cd ..
+rm -rf dist
+cp -r ts/dist dist
+
+for dir in js/*/; do
+  cp -r "${dir%/}" dist/
+done
+
