d3ca02998624747f9ea12c0b59ca8ec632e6df5f
yahiro <yahiro07@users.noreply.github.com>
2026-08-03 21:45:38 +0900

    [tinysynth] add program shift button

--- js/webaudio-tinysynth-mini/app.js
+++ js/webaudio-tinysynth-mini/app.js
@@ -57,7 +57,7 @@ async function Init() {
 }
 
 function Ctrl() {
-  if (typeof synth != "undefined") {
+  if (typeof synth !== "undefined") {
     synth.masterVol = document.getElementById("vol").value;
     synth.reverbLev = document.getElementById("rev").value;
   }
@@ -72,6 +72,14 @@ function KeyIn(e) {
 
 function ProgChange(p) {
   synth.send([0xc0, p]);
+}
+
+function ProgShift(dir) {
+  const sel = document.getElementById("prog");
+  const n = sel.options.length;
+  const next = (((sel.selectedIndex + dir) % n) + n) % n;
+  sel.selectedIndex = next;
+  ProgChange(next);
 }
 
 function SetQuality(n) {
